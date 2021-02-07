34b5e806a5d2bc33e4d8b6bb6aa9d3277bc505cd
Lucas Vieira <lucasltv@gmail.com>
2021-02-07 13:08:53 -0300

    sqs handler

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("asdasd")
+require("dotenv").config();
+const { Consumer } = require("sqs-consumer");
+const { SQS } = require("aws-sdk");
+
+const { AWS_QUEUE_URL, AWS_SECRET_KEY, AWS_ACCESS_KEY } = process.env;
+
+const queueConsumer = Consumer.create({
+  queueUrl: AWS_QUEUE_URL,
+  handleMessage,
+  sqs: new SQS({
+    region: "us-east-1",
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+  }),
+});
+queueConsumer.on("error", console.error);
+queueConsumer.on("processing_error", console.error);
+queueConsumer.on("timeout_error", console.error);
+queueConsumer.start();
+
+async function handleMessage(sqsMessage) {
+  try {
+    // const message = JSON.parse(sqsMessage.Body)
+    console.log("LOG ~ handleMessage ~ sqsMessage.Body", sqsMessage.Body);
+  } catch (err) {
+    console.log("LOG ~ handleMessage ~ err", err);
+  }
+}
+
+console.log(`Worker started at ${new Date()}`);
