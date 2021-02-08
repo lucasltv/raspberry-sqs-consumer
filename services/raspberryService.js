00cdf644a46f22649328b3969a89018baf63ca77
Lucas Vieira <lucasltv@gmail.com>
2021-02-07 21:41:57 -0300

    output controll

--- services/raspberryService.js
+++ services/raspberryService.js
@@ -1,5 +1,20 @@
+const Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+const LED = new Gpio(4, "out"); //use GPIO pin 4, and specify that it is output
+
 module.exports = {
   handleSqsCommand: async function (message) {
-    console.log(`LOG ~ handleSqsCommand ~ sqsCommand`, message);
+    if (!Object.prototype.hasOwnProperty.call(message, "enabled")) {
+      throw new Error(
+        `Parameter 'outputEnabled' not found! \n Message received: ${JSON.stringify(
+          message
+        )}`
+      );
+    }
+    const { outputEnabled } = message;
+    if (outputEnabled) {
+      LED.writeSync(1);
+    } else {
+      LED.writeSync(0);
+    }
   },
 };
